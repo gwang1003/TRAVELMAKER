67cecae2a7f25c453ba9ed8034885c636164f74d
rhxo9 <rhxo9@LAPTOP-SCATRHI0>
2019-12-10 20:12:32 +0900

    112233

--- TravelMaker/src/board/controller/ThumbnailListServlet.java
+++ TravelMaker/src/board/controller/ThumbnailListServlet.java
@@ -1,11 +1,17 @@
 package board.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import board.model.service.BoardService;
+import board.model.vo.Attachment;
+import board.model.vo.Board;
 
 /**
  * Servlet implementation class ThumbnailListServlet
@@ -28,6 +34,11 @@ public class ThumbnailListServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		BoardService bs = new BoardService();
+		
+		ArrayList<Board> blist = bs.selectList(1);
+		ArrayList<Attachment> flist = bs.selectList(2);
 	}
 
 	/**
--- TravelMaker/src/board/model/service/BoardService.java
+++ TravelMaker/src/board/model/service/BoardService.java
@@ -19,15 +19,15 @@ public class BoardService {
 	public int getListCount() {
 		Connection con = getConnection();
 		int result = new BoardDao().getListCount(con);
-		
+
 		return 0;
 	}
 
 	// 2. 게시글 리스트 조회용 서비스 메소드
 	public ArrayList<Board> selectList(int currentPage, int boardLimit) {
 		Connection con = getConnection();
-		ArrayList<Board> list = new BoardDao().selectBList(con,currentPage,boardLimit);
-		
+		ArrayList<Board> list = new BoardDao().selectBList(con, currentPage, boardLimit);
+
 		return new ArrayList<Board>();
 
 	}
@@ -35,28 +35,28 @@ public class BoardService {
 	// 3. 게시판 상세보기(조회수 증가)
 	public Board selectBoard(int bid) {
 		Connection con = getConnection();
-		Board b = new BoardDao().selectBoard(con , bid);
+		Board b = new BoardDao().selectBoard(con, bid);
 		return new Board();
 	}
 
 	// 4. 게시판 상세보기(조회수 증가 없이)
 	public Board selectBoardNoCnt(int bid) {
 		Connection con = getConnection();
-		Board b = new BoardDao().selectBoardNoCnt(con , bid);
+		Board b = new BoardDao().selectBoardNoCnt(con, bid);
 		return new Board();
 	}
 
 	// 5. 게시글 삭제 서비스
 	public int deleteBoard(int bid) {
 		Connection con = getConnection();
-		int result = new BoardDao().deleteBoard(con,bid);
+		int result = new BoardDao().deleteBoard(con, bid);
 		return 0;
 	}
 
 	// 6. 게시글 입력용 서비스
 	public int insertBoard(Board b) {
 		Connection con = getConnection();
-		int result = new BoardDao().insertBoard(con,b);
+		int result = new BoardDao().insertBoard(con, b);
 		return 0;
 	}
 
@@ -64,7 +64,7 @@ public class BoardService {
 	public int updateBoard(Board b) {
 
 		Connection con = getConnection();
-		int result = new BoardDao().updateBoard(con,b);
+		int result = new BoardDao().updateBoard(con, b);
 		return 0;
 	}
 
@@ -73,37 +73,47 @@ public class BoardService {
 	// 2인 경우 메인 사진 리스트가 리턴
 	public ArrayList selectList(int flag) {
 		Connection con = getConnection();
-		ArrayList list = new BoardDao().selectFList(con,flag);
-		return new ArrayList();
+		ArrayList list = null;
+
+		BoardDao bDao = new BoardDao();
+
+		if (flag == 1) {
+			list = bDao.selectBList(con);
+		} else {
+			list = bDao.selectFList(con);
+		}
+
+		close(con);
+
+		return list;
 	}
 
 	// 사진 게시판 글쓰기
 	public int insertThumbnail(Board b, ArrayList<Attachment> fileList) {
 		Connection con = getConnection();
-		int result = new BoardDao().insertThumbnail(con,b,fileList);
+		int result = new BoardDao().insertThumbnail(con, b, fileList);
 		return 0;
 	}
 
 	// 사진 게시판 상세보기
 	public ArrayList<Attachment> selectThumbnail(int bId) {
 		Connection con = getConnection();
-		ArrayList<Attachment> list = new BoardDao().selectThumbnail(con,bId);
+		ArrayList<Attachment> list = new BoardDao().selectThumbnail(con, bId);
 		return new ArrayList<Attachment>();
 	}
-	
-	public ArrayList<Reply> selectReplyList(int bId){
+
+	public ArrayList<Reply> selectReplyList(int bId) {
 		Connection con = getConnection();
-		ArrayList<Reply> list = new BoardDao().selectReplyList(con,bId);
-		
+		ArrayList<Reply> list = new BoardDao().selectReplyList(con, bId);
+
 		return new ArrayList<Reply>();
-		
-		
+
 	}
-	
-	public ArrayList<Reply> insertReply(Reply r){
+
+	public ArrayList<Reply> insertReply(Reply r) {
 		Connection con = getConnection();
 		ArrayList<Reply> list = new BoardDao().insertReply(con);
-		
+
 		return new ArrayList<Reply>();
 	}
 
